6c7d8240fd4c8813521081b7c11be49c780dcef1
kyaloeriq <kyaloeriq@gmail.com>
2024-08-20 11:29:43 -0700

    class FIFOCache that inherits from BaseCaching and is a caching system

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+FIFOCache module
+"""
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    FIFOCache class that inherits from BaseCaching
+    Implements a FIFO caching system.
+    """
+
+    def __init__(self):
+        """
+        Initialize the class and call the parent constructor.
+        """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """
+        Assigns the item value to the key in the cache_data dictionary.
+        If key or item is None, this method does nothing.
+        If the number of items in cache_data exceeds BaseCaching.MAX_ITEMS,
+        it discards the first item put in the cache using FIFO and prints DISCARD.
+        """
+        if key is not None and item is not None:
+            if key not in self.cache_data:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    first_key = self.order.pop(0)
+                    del self.cache_data[first_key]
+                    print(f"DISCARD: {first_key}")
+                self.order.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Returns the value in cache_data linked to key.
+        If key is None or doesn't exist in cache_data, returns None.
+        """
+        return self.cache_data.get(key, None)
