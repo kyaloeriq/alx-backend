f2e3c6875eb877fc08a251e8779830766b1e3324
kyaloeriq <kyaloeriq@gmail.com>
2024-08-27 10:10:51 -0700

    basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Basic Flask app setup
+This module sets up a basic Flask application with a single route.
 """
 
 from flask import Flask, render_template
@@ -10,8 +10,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    Renders the index.html template.
+    """
     return render_template('index.html')
 
 
 if __name__ == "__main__":
+    """
+    Runs the Flask application on host 0.0.0.0 and port 5000.
+    """
     app.run(host="0.0.0.0", port=5000)
